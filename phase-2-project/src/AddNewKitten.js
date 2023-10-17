937e02a10e8436c091e34b4901677cd373176f64
Charlie Gray <charliechasegray@gmail.com>
2023-10-17 08:51:48 -0700

    create form and handleSubmit function and style

--- phase-2-project/src/AddNewKitten.js
+++ phase-2-project/src/AddNewKitten.js
@@ -1,14 +1,29 @@
 import React, {useId} from "react";
 
 function AddNewKitten() {
-
+    function handleSubmit() {
+        console.log("Hi")
+    }
     
     return (
-        <form>
-            <label>
-
-            </label>
-        </form>
+        <div className="new-kitten-form">
+            <h2 className="form-title">
+                Add New Kitten to Collection
+            </h2>
+            <form onSubmit={handleSubmit}>
+                <label className="label-text">
+                    Color
+                    <input type="text" placeholder="Kitten Fur Color..."/>
+                </label>
+            
+                <label className="label-text">
+                    Image Url
+                    <input type="text" placeholder="Kitten Image URL..."/>
+                </label>
+                <input type="submit" className="submit" value="submit"/>
+            </form>
+        </div>
+        
     )
 }
 
